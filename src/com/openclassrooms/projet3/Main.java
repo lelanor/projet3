cfad3e73be204dc4b59c19d6a12b7b7b3e33bdbc
enrico <elofaro@outlook.fr>
2018-04-28 15:55:55 +0200

    finalisation de la migration de l'ancien main() vers la classe Game

--- src/com/openclassrooms/projet3/Main.java
+++ src/com/openclassrooms/projet3/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         Game game = new Game();
+
         game.initialDataRequest();
+        game.setPlayersAndCode();
+        game.playGame();
     }
 }
